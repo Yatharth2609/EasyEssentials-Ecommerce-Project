12cde2d0a05977339f07972f78d9f556b3e24fcf
Yatharth2609 <yatharth.mishra2002@gmail.com>
2023-09-30 20:55:19 +0530

    redux implemented

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,29 +1,10 @@
 import e from "express";
 import express from "express";
+import { getProducts, getProductById } from '../../backend/controllers/productController.js';
+
 const router = express.Router();
-import asyncHandler from "../middleware/asyncHandler.js";
-import Product from "../models/productModel.js";
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      res.json(product);
-    } else {
-        res.status(404);
-      throw new Error("Product Not Found!!");
-    }
-  })
-);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
